71e91a61a1abd80d4e7739a051bf62dc5844aea5
qingquaner <897546244@qq.com>
2018-12-21 08:43:44 +0800

    init

---
+++ src/main/java/com/zzhb/zzoa/controller/xtgl/ZzglController.java
@@ -0,0 +1,13 @@
+package com.zzhb.zzoa.controller.xtgl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//组织管理
+@Controller
+@RequestMapping("/zzgl")
+public class ZzglController {
+
+
+}
---
+++ src/main/java/com/zzhb/zzoa/exception/Global500ExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.zzhb.zzoa.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zzhb.zzoa.ZzoaApplication;
+
+@ControllerAdvice
+public class Global500ExceptionHandler {
+
+	private static Logger logger = LoggerFactory.getLogger(ZzoaApplication.class);
+
+	@ExceptionHandler(value = Exception.class)
+	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) {
+		ModelAndView mv = new ModelAndView("error/500");
+		e.printStackTrace();
+		logger.error(req.getRequestURI() + "========" + e.toString());
+		mv.addObject("msg", e.toString());
+		return mv;
+	}
+}
