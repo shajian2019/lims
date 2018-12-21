48ec68adc9f488195c6b9e8ada75e9a8d14f793c
qingquaner <897546244@qq.com>
2018-12-21 10:08:09 +0800

    init

--- src/main/java/com/zzhb/zzoa/controller/xtgl/ZzglController.java
+++ src/main/java/com/zzhb/zzoa/controller/xtgl/ZzglController.java
@@ -1,13 +1,16 @@
 package com.zzhb.zzoa.controller.xtgl;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 //组织管理
 @Controller
 @RequestMapping("/zzgl")
 public class ZzglController {
 
-
+	@GetMapping("/yhgl")
+	public String yhgl() {
+		return "xtgl/zzgl/yhgl/yhgl";
+	}
 }
