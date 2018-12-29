d83ccd6f3045d85308c53bbdba4e7a9b21e686c6
qingquaner <897546244@qq.com>
2018-12-29 18:47:08 +0800

    init

--- src/main/java/com/zzhb/zzoa/controller/lcgl/BsglController.java
+++ src/main/java/com/zzhb/zzoa/controller/lcgl/BsglController.java
@@ -1,13 +1,8 @@
 package com.zzhb.zzoa.controller.lcgl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.alibaba.fastjson.JSONObject;
-import com.zzhb.zzoa.service.ActivitiService;
 
 //部署管理
 @Controller
@@ -19,12 +14,4 @@ public class BsglController {
 		return "lcgl/bsgl/bsgl";
 	}
 
-	@Autowired
-	ActivitiService activitiService;
-
-	@GetMapping("/list")
-	@ResponseBody
-	public JSONObject bsglList() {
-		return activitiService.listDeloy(1, 20);
-	}
 }
--- src/main/java/com/zzhb/zzoa/service/ActivitiService.java
+++ src/main/java/com/zzhb/zzoa/service/ActivitiService.java
@@ -1,19 +1,10 @@
 package com.zzhb.zzoa.service;
 
-import java.util.List;
-
 import org.activiti.engine.RepositoryService;
-import org.activiti.engine.repository.Deployment;
-import org.activiti.engine.repository.ProcessDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.alibaba.fastjson.JSONObject;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.zzhb.zzoa.utils.LayUiUtil;
 
 @Service
 public class ActivitiService {
@@ -23,11 +14,5 @@ public class ActivitiService {
 	@Autowired
 	RepositoryService repositoryService;
 
-	public JSONObject listDeloy(Integer page, Integer limit) {
-		PageHelper.startPage(page, limit);
-		List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery().list();
-		PageInfo<ProcessDefinition> pageInfo = new PageInfo<>(list);
-		return LayUiUtil.pagination(pageInfo);
-	}
 
 }
