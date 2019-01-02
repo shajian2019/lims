6dfae49a5ad387fe4b32894685401e7043a3f985
chj <897546244@qq.com>
2019-01-02 11:36:05 +0800

    init

--- src/main/java/com/zzhb/zzoa/ZzoaApplication.java
+++ src/main/java/com/zzhb/zzoa/ZzoaApplication.java
@@ -18,7 +18,6 @@ public class ZzoaApplication {
 
 	private static Logger logger = LoggerFactory.getLogger(ZzoaApplication.class);
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ZzoaApplication.class, args);
 		logger.info("======ZzoaApplication===start success==");
