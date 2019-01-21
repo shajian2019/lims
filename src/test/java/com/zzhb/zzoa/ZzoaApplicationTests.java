c47ac1daa6b17d88740a8906b6fd40faed6c819f
chj <897546244@qq.com>
2019-01-21 16:45:58 +0800

    添加注释

--- src/test/java/com/zzhb/zzoa/ZzoaApplicationTests.java
+++ src/test/java/com/zzhb/zzoa/ZzoaApplicationTests.java
@@ -286,4 +286,12 @@ public class ZzoaApplicationTests {
 		}
 		return highLightedFlowIds;
 	}
+	
+	
+	@Test //中止与激活流程
+	public void testSupend() {
+		String processInstanceId = "160011";
+//		rs.suspendProcessInstanceById(processInstanceId);
+		rs.activateProcessInstanceById(processInstanceId);
+	}
 }
