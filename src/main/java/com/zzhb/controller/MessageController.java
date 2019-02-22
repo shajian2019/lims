5ed9206b1040574c5320f87c2c3d38a541f408c4
chj <897546244@qq.com>
2019-02-22 09:49:53 +0800

    1

--- src/main/java/com/zzhb/controller/MessageController.java
+++ src/main/java/com/zzhb/controller/MessageController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,5 +29,17 @@ public class MessageController {
 	@ResponseBody
 	public JSONObject getMessages(Integer page, Integer limit, @RequestParam Map<String, String> params) {
 		return messageService.getMessages(page, limit, params);
+	}
+
+	@PostMapping("/update")
+	@ResponseBody
+	public void updateMessage(@RequestParam Map<String, Object> params) {
+		messageService.updateMessage(params);
+	}
+	
+	@PostMapping("/delete")
+	@ResponseBody
+	public void deleteMessage(@RequestParam Map<String, Object> params) {
+		messageService.deleteMessage(params);
 	}
 }
